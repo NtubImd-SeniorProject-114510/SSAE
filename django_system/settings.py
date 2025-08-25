818514cccf0a03d2a7707c9c4bda225368d3689d
11056030 <11056030@ntub.edu.tw>
2025-08-25 22:01:01 +0800

    修改 - 活動可使用地圖
    修改 - 活動可使用地圖

--- django_system/settings.py
+++ django_system/settings.py
@@ -195,7 +195,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "web" /"static",
 ]
 
 
--- web_app/forms.py
+++ web_app/forms.py
@@ -6,21 +6,30 @@ class ActivityForm(forms.ModelForm):
     class Meta:
         model = GroupActivity
         fields = [
-            'title', 'type', 'location', 'date', 'time',
-            'min_participants', 'max_participants',
-            'description', 'cover_image', 'deadline'
+            'title', 'type', 'location', 'address', 'latitude', 'longitude', 'place_id',
+            'date', 'time', 'min_participants', 'max_participants',
+            'description', 'cover_image', 'deadline', 'contact_info'
         ]
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
             'time': forms.TimeInput(attrs={'type': 'time'}),
             'deadline': forms.DateInput(attrs={'type': 'date'}),
-            'cover_image': forms.FileInput(attrs={'required': False}), # 讓圖片變成非必填
+            'cover_image': forms.FileInput(attrs={'required': False}),
+            'address': forms.HiddenInput(),
+            'latitude': forms.HiddenInput(),
+            'longitude': forms.HiddenInput(),
+            'place_id': forms.HiddenInput(),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # 確保圖片欄位不是必填
         self.fields['cover_image'].required = False
+        self.fields['address'].required = False
+        self.fields['latitude'].required = False
+        self.fields['longitude'].required = False
+        self.fields['place_id'].required = False
+        self.fields['contact_info'].required = False
 
     def clean(self):
         cleaned_data = super().clean()
